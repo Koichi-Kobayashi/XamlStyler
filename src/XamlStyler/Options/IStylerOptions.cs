56aedaee5890fd5e1647267260f85dcc1987e0b4
Koichi Kobayashi <Koichi-Kobayashi@users.noreply.github.com>
2026-01-07 14:22:45 +0900

    Revert "Add option to treat comments starting with '<' as multiline (#380)"
    This reverts commit f03a86413fa8f209fd4cde1e3e51bf8652cc012d.

--- src/XamlStyler/Options/IStylerOptions.cs
+++ src/XamlStyler/Options/IStylerOptions.cs
@@ -107,8 +107,6 @@ namespace Xavalon.XamlStyler.Options
 
         int CommentSpaces { get; set; }
 
-        bool TreatCommentWithTagAsMultiline { get; set; }
-
         #endregion Misc
 
         #region Configuration
